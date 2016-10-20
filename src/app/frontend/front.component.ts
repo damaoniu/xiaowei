f87db49d6bf5380925a5a05a548a1ed3fbf83dd1
= <maoniuwork@gmail.com>
2016-10-20 13:44:36 -0400

    add mobile menu

--- src/app/frontend/front.component.ts
+++ src/app/frontend/front.component.ts
@@ -50,7 +50,74 @@ export class FrontComponent implements OnInit,AfterContentInit{
             $j('header .badge').removeClass('badge--hidden');
         });
 
+        //mobile menu
 
+            if ($j("#off-canvas-menu").length > 0) {
+                "use strict";
+                $j(document).bind('cbox_open', function() {
+                    $j('html').css({
+                        overflow: 'hidden'
+                    });
+                }).bind('cbox_closed', function() {
+                    $j('html').css({
+                        overflow: ''
+                    });
+                });
+
+
+                // check if RTL mode
+                var colorBoxMenuPosL = ($j("body").hasClass('rtl')) ? 'none' : '0';
+                var colorBoxMenuPosR = ($j("body").hasClass('rtl')) ? '0' : 'none';
+
+                $j(".off-canvas-menu-toggle").colorbox({
+                    inline: true,
+                    opacity: 0.55,
+                    transition: "none",
+                    arrowKey: false,
+                    width: "300",
+                    height: "100%",
+                    fixed: true,
+                    className: 'canvas-menu',
+                    top: 0,
+                    right: colorBoxMenuPosR,
+                    left: colorBoxMenuPosL,
+                    colorBoxCartPos: 0
+                })
+            }
+
+
+        $j(window).resize(function(){
+            var $cboxClose = $j("#cboxClose");
+            if($cboxClose.length && window.innerWidth > 1024 ) {
+                $j("#cboxClose").trigger("click");
+            }
+        })
+
+
+
+            $j("#off-canvas-menu .expander-list").find("ul").hide().end().find(" .expander").text("+").end().find(".active").each(function() {
+                $j(this).parents("li ").each(function() {
+                    var $jthis = $j(this),
+                        $jul = $jthis.find("> ul"),
+
+                        $jexpander = $jthis.find("> .name .expander");
+                    $jul.show();
+
+                    $jexpander.html("&minus;")
+                })
+            }).end().find(" .expander").each(function() {
+                var $jthis = $j(this),
+                    hide = $jthis.text() === "+",
+                    $jul = $jthis.parent(".name").next("ul"),
+                    $jname = $jthis.next("a");
+                $jthis.click(function() {
+                    if ($jul.css("display") ==
+                        "block") $jul.slideUp("slow");
+                    else $jul.slideDown("slow");
+                    $j(this).html(hide ? "&minus;" : "+");
+                    hide = !hide
+                })
+            })
 
     }
     ngAfterContentInit():void {
