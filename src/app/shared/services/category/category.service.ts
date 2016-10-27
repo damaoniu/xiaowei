5757b946adf655f18778db968a43d73e5a6f907b
= <maoniuwork@gmail.com>
2016-10-27 14:35:31 -0400

    categories services intitated

--- src/app/shared/services/category/category.service.ts
+++ src/app/shared/services/category/category.service.ts
@@ -1,5 +1,25 @@
 import {Injectable} from "@angular/core";
-@Injectable()
-export class CategoryService{
+import {BaseService} from "../BaseService.service";
+import {Http} from "@angular/http";
+import {Config} from "protractor/built/index";
+import 'rxjs';
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/map";
 
+@Injectable()
+export class CategoryService extends BaseService{
+    constructor(http:Http){
+        super(http);
+    }
+    getFirstLevel(){
+        return this._http.get(Config.categoryServiceUrl+"/firstLevelCategories").map(res=>res.json())
+            .map(res=>res['categories'])
+            .catch(this._handleErrors);
+    }
+    getSecondLevelCategory(firstLevelId:string){
+        return this._http.get(Config.categorServiceUrl+"/secondLevelCategories")
+            .map(res=>res.json())
+            .map(res=>res['categories'])
+            .catch(this._handleErrors);
+    }
 }
--- src/app/shared/services/config.ts
+++ src/app/shared/services/config.ts
@@ -1,5 +1,7 @@
 export class Config {
-  static webServiceUrl="https://dev.xianfengzu.com/";
+  static webServiceUrl="localhost:9001";
+  static productServiceUrl="localhost:9001";
+  static categoryServiceUrl="localhost:9002";
   static token="adf";
   static mediaServerUrl="https://dev.xianfengzu.com/pms-media-service/";
 
