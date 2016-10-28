0c038dfcb2a4599828e8da2c0910e9b0e86321cd
= <maoniuwork@gmail.com>
2016-10-28 11:49:23 -0400

    made revslider into a component

--- src/app/frontend/components/parts/parts.module.ts
+++ src/app/frontend/components/parts/parts.module.ts
@@ -3,10 +3,11 @@ import {Cart} from "./cart/cart.component";
 import {SharedModule} from "../../../shared/sharedmodule";
 import {SlideToggle} from "./toggleSlide/toggleSlide.component";
 import {AddToCart} from "./addToCart/addToCart.component";
+import {RevSliderComponent} from "./revSlider/revSlider.component";
 @NgModule({
     imports:[SharedModule],
-    declarations:[Cart,SlideToggle,AddToCart],
-    exports:[Cart,SlideToggle,AddToCart]
+    declarations:[Cart,SlideToggle,AddToCart,RevSliderComponent],
+    exports:[Cart,SlideToggle,AddToCart,RevSliderComponent]
 })
 export class PartsModule{
 
---
+++ src/app/frontend/components/parts/revSlider/revSlider.component.ts
@@ -0,0 +1,120 @@
+import {
+    Component, ChangeDetectionStrategy, Input, ElementRef, NgZone, ViewEncapsulation,
+    ViewChild, Renderer
+} from "@angular/core";
+import {Observable} from "rxjs/Rx";
+declare var jQuery:any;
+let $j=jQuery.noConflict();
+@Component({
+        selector:"revSlider",
+        templateUrl:"./revSlider.html",
+        changeDetection:ChangeDetectionStrategy.OnPush,
+        encapsulation:ViewEncapsulation.None
+    })
+export class RevSliderComponent{
+    @Input('slides') slides:Observable<any>;
+    @Input() wrapperClass:string='';
+    @ViewChild('sliders') sliders:ElementRef;
+    constructor(public el:ElementRef,private  renderer:Renderer){}
+
+    // ngOnInit(){
+    //     let that=this;
+    //     this.slides.subscribe((data)=>{
+    //         setTimeout(()=>{
+    //             that.buildSlider();
+    //         })
+    //     })
+    // }
+    buildSlider(){
+        var windowW = window.innerWidth || $j(window).width();
+        var fullwidth;
+        var fullscreen;
+        jQuery(window).resize(sliderOptions);
+        sliderOptions();
+        function sliderOptions(){
+            if (windowW > 767) {
+                fullwidth = "off";
+                fullscreen = "on";
+            } else {
+                fullwidth = "on";
+                fullscreen = "off";
+            }
+        }
+        console.log(this.sliders.nativeElement);
+        $j(this.sliders.nativeElement).show().revolution({
+            dottedOverlay:"none",
+            delay:1600,
+            startwidth:2048,
+            startheight:900,
+            hideThumbs:200,
+            hideTimerBar:"on",
+
+            thumbWidth:100,
+            thumbHeight:50,
+            thumbAmount:5,
+
+            navigationType:"none",
+            navigationArrows:"",
+            navigationStyle:"",
+
+            touchenabled:"on",
+            onHoverStop:"on",
+
+            swipe_velocity: 0.7,
+            swipe_min_touches: 1,
+            swipe_max_touches: 1,
+            drag_block_vertical: false,
+
+            parallax:"mouse",
+            parallaxBgFreeze:"on",
+            parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
+
+            keyboardNavigation:"off",
+
+            navigationHAlign:"center",
+            navigationVAlign:"bottom",
+            navigationHOffset:0,
+            navigationVOffset:20,
+
+            soloArrowLeftHalign:"left",
+            soloArrowLeftValign:"center",
+            soloArrowLeftHOffset:20,
+            soloArrowLeftVOffset:0,
+
+            soloArrowRightHalign:"right",
+            soloArrowRightValign:"center",
+            soloArrowRightHOffset:20,
+            soloArrowRightVOffset:0,
+
+            shadow:0,
+            fullWidth: fullwidth,
+            fullScreen: fullscreen,
+
+            spinner:"",
+            h_align:"left",
+
+            stopLoop:"off",
+            stopAfterLoops:-1,
+            stopAtSlide:-1,
+
+            shuffle:"off",
+
+            autoHeight:"off",
+            forceFullWidth:"off",
+
+
+            hideThumbsOnMobile:"off",
+            hideNavDelayOnMobile:1500,
+            hideBulletsOnMobile:"off",
+            hideArrowsOnMobile:"off",
+            hideThumbsUnderResolution:0,
+
+            hideSliderAtLimit:0,
+            hideCaptionAtLimit:0,
+            hideAllCaptionAtLilmit:0,
+            startWithSlide:0,
+            fullScreenOffsetContainer: "#header"
+        });
+        this.renderer.setElementStyle(this.el.nativeElement,"display","block");
+    }
+}
